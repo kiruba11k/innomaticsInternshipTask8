cbd0a8a1d04548a05ea33ee4de011825512db220
kiruba11k <kiruba11geo@gmail.com>
2024-10-28 21:48:00 +0530

    Add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
     <App />
-  </React.StrictMode>
-);
+)
